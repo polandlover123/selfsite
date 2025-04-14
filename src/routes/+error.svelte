131108ca810c682e1287d0c0173b6787d680329d
caernarveron <109875196+caernarferon@users.noreply.github.com>
2025-04-14 23:50:49 +0000

    fix
    Signed-off-by: parliamentbomber <109875196+caernarferon@users.noreply.github.com>

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,29 +8,6 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
-  }
-  h1 {
-    color: #dde1e6;
-  }
-  a {
-    color: #dde1e6;
-    font-size: 1rem;
-    padding: 0.5rem 1rem;
-  }
-</style>
-
-
-<div class="errorcontainer">
-  <h1>whoopsies, you shouldn't be here</h1>
-  <a href="/">go back</a>
-</div>
-
-<style>
-  :root {
-    display: flex;
-    justify-content: center;
-    align-items: center;
     min-height: 100vh;
     overflow:hidden;
   }
@@ -43,4 +20,3 @@
     padding: 0.5rem 1rem;
   }
 </style>
-
