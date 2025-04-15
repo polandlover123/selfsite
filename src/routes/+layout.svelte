3309d337c94634a940ec24d685c606a8f5a5f653
caernarveron <109875196+polandlover123@users.noreply.github.com>
2025-04-15 04:11:17 +0000

    fix because i forgor something

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "victormono";
   import "../app.css";
   import { injectAnalytics } from '@vercel/analytics/sveltekit'
+  import { dev } from '$app/environment';
   let { children } = $props();
   injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
