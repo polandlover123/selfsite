27fa322cb0dc04378f1d370f847f4b5ae6c3831f
caernarveron <109875196+polandlover123@users.noreply.github.com>
2025-04-15 02:25:28 +0000

    yes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <div class="container">
   <div class="title">
-    <h1><i>hello</i></h1>
+    <h1 class="hellotitle"><i>hello!</i></h1>
+  </div>
+  <div class="polandlover">
+    <h1 class="selftitle">
+      I am polandlover123
+    </h1>
   </div>
   <div class="about">
-    <h1>i like nix/nixos and cybersecurity. i also like web development and linux.</h1>
+    <h1 class="yaptitle">i like nix/nixos and cybersecurity. i also like web development and linux.</h1>
   </div>
 </div>
